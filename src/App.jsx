17ca033eaeb42dea7ac6c6f75f88f2a880a038af
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-09-09 12:37:19 +0200

    Add checkbox keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 function PixelGrid() {
-  const grid = [...Array(7)].map(() =>
-    [...Array(5)].map(() => <input type="checkbox" />),
+  const COLUMN_COUNT = 5;
+  const ROW_COUNT = 8;
+
+  const grid = [...Array(ROW_COUNT).keys()].map((key1) =>
+    [...Array(COLUMN_COUNT).keys()].map((key2) => (
+      <input key={key1 * COLUMN_COUNT + key2} type="checkbox" />
+    )),
   );
 
   return <div className="pixel-grid">{grid}</div>;
