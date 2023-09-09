7062c846acfd52fea5198ba5996aab185a6b1d46
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-09-09 12:53:30 +0200

    Make pixel grid interactive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+
 function PixelGrid() {
   const COLUMN_COUNT = 5;
   const ROW_COUNT = 8;
 
+  const [pixels, setPixels] = useState(
+    Array(COLUMN_COUNT * ROW_COUNT).fill(false),
+  );
+
+  function updatePixel(e, id) {
+    setPixels((pixels) => {
+      pixels[id] = e.target.checked;
+      return [...pixels];
+    });
+  }
+
   const grid = [...Array(ROW_COUNT).keys()].map((key1) =>
-    [...Array(COLUMN_COUNT).keys()].map((key2) => (
-      <input key={key1 * COLUMN_COUNT + key2} type="checkbox" />
-    )),
+    [...Array(COLUMN_COUNT).keys()].map((key2) => {
+      const id = key1 * COLUMN_COUNT + key2;
+
+      return (
+        <input
+          checked={pixels[id]}
+          key={id}
+          onChange={(e) => updatePixel(e, id)}
+          type="checkbox"
+        />
+      );
+    }),
   );
 
   return <div className="pixel-grid">{grid}</div>;
